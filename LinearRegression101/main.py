6c31664f1cb8cca184ec697a63778ed4588bee0b
MariemDaoud1 <mariem.daoud@insat.ucar.tn>
2025-09-21 12:35:06 +0100

    LinearRegression

---
+++ LinearRegression101/main.py
@@ -0,0 +1,13 @@
+# Imports
+import pandas as pd
+import numpy as np
+print(np.__version__)
+
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+
+data = pd.read_csv("student-mat.csv", sep=";")
+print(data.head())
+data = data [["G1", "G2", "G3", "studytime", "failures", "absences"]]
